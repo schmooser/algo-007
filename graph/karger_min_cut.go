fd5786f7552a797ca65ca9f1d36ce0e2a4c73b27
Pavel Popov <pavelpopov@outlook.com>
2015-02-18 21:02:36 +0300

    Fix Karger MinCut algorithm
    There was a problem with self-loops deletion. Fix it by changing
    removing duplicates to creating new slice withou duplicates.
    
    Also change indentation accordingly coding standards, replace
    spaces to tabs.

--- graph/karger_min_cut.go
+++ graph/karger_min_cut.go
@@ -7,161 +7,146 @@ Author: Pavel Popov <pavelpopov@outlook.com>
 package graph
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
-
 type Edge struct {
-    From, To int
+	From, To int
 }
 
 type Vortex struct {
-    X int
+	X int
 }
 
 type Vortices []Vortex
 type Edges []Edge
 
-
 func init() {
-    rand.Seed(time.Now().UTC().UnixNano())
-    //rand.Seed(13)
+	rand.Seed(time.Now().UTC().UnixNano())
+	//rand.Seed(13)
 }
-
 
 func MakeGraph(rows [][]int) (vortices Vortices, edges Edges) {
-    /*
-    input rows contain data for a graph:
-        1 2 4 5
-        2 1
-        3 4 5
-        4 1 3
-        5 1 3
-    First column is a Vertex number.
-    Other is connected Vertexes.
-    */
+	/*
+	   input rows contain data for a graph:
+	       1 2 4 5
+	       2 1
+	       3 4 5
+	       4 1 3
+	       5 1 3
+	   First column is a Vertex number.
+	   Other is connected Vertexes.
+	*/
 
+	/*
+	   var rows [][]int;
+	   rows = append(rows, []int{1, 2, 4, 5})
+	   rows = append(rows, []int{2, 1})
+	   rows = append(rows, []int{3, 4, 5})
+	   rows = append(rows, []int{4, 1, 3})
+	   rows = append(rows, []int{5, 1, 3})
+	*/
 
-    /*
-    var rows [][]int;
-    rows = append(rows, []int{1, 2, 4, 5})
-    rows = append(rows, []int{2, 1})
-    rows = append(rows, []int{3, 4, 5})
-    rows = append(rows, []int{4, 1, 3})
-    rows = append(rows, []int{5, 1, 3})
-    */
+	//rows, _ := util.ReadGraph(filename)
 
-    //rows, _ := util.ReadGraph(filename)
+	for _, row := range rows {
+		vortex := Vortex{row[0]}
+		vortices = append(vortices, vortex)
+		for _, x := range row {
+			if x > vortex.X {
+				edges = append(edges, Edge{vortex.X, x})
+			}
+		}
+	}
 
-    for _, row := range rows {
-        vortex := Vortex{row[0]}
-        vortices = append(vortices, vortex)
-        for _, x := range row {
-            if x > vortex.X {
-                edges = append(edges, Edge{vortex.X, x})
-            }
-        }
-    }
-
-    return
+	return
 }
 
-
-func swapWithLast(edges []Edge, pos int) []Edge {
-    return edges
+func (vortices Vortices) RemoveVortex(k int) Vortices {
+	l := len(vortices) - 1
+	_, vortices[k] = vortices[k], vortices[l]
+	return vortices[:l]
 }
-
-
-func (vortices Vortices) removeVortex(k int) (vv Vortices, err error) {
-    l := len(vortices)-1
-    _, vortices[k] = vortices[k], vortices[l]
-    vv = vortices[:l]
-    err = nil
-    return
-}
-
 
 func KargerContraction(vortices Vortices, edges []Edge) (Vortices, []Edge) {
 
+	if len(vortices) == 2 {
+		return vortices, edges
+	}
 
-    if len(vortices) == 2 {
-        return vortices, edges
-    }
+	var print bool = false
 
-    var print bool = false
+	if len(vortices) < 0 {
+		print = true
+	}
 
-    if len(vortices) < 0 {
-        print = true
-    }
+	if print {
+		fmt.Println("KargerContraction invoked")
+		fmt.Printf("Len(vortices) = %d\n", len(vortices))
 
-    if print {
-        fmt.Println("KargerContraction invoked")
-        fmt.Printf("Len(vortices) = %d\n", len(vortices))
+		fmt.Println("Vortices")
+		fmt.Println(vortices)
 
-        fmt.Println("Vortices")
-        fmt.Println(vortices)
+		fmt.Println("Edges")
+		fmt.Println(edges)
+	}
 
-        fmt.Println("Edges")
-        fmt.Println(edges)
-    }
+	//fmt.Println(edges)
+	k := rand.Intn(len(edges))
 
-    k := rand.Intn(len(edges))
+	// swap chosen edge with the last in slice, x - contracted edge
+	var x Edge
 
-    // swap chosen edge with the last in slice, x - contracted edge
-    var x Edge
+	edges[k], x = edges[len(edges)-1], edges[k]
+	edges = edges[:len(edges)-1]
 
-    edges[k], x = edges[len(edges)-1], edges[k]
-    edges = edges[:len(edges)-1]
+	if print {
+		fmt.Println("Selected edge")
+		fmt.Println(x)
+	}
 
-    if print {
-        fmt.Println("Selected edge")
-        fmt.Println(x)
-    }
+	for i, v := range vortices {
+		if v.X == x.To { // remove vortex
+			vortices = vortices.RemoveVortex(i)
+			break
+		}
+	}
 
-    for i, v := range vortices {
-        if v.X == x.To { // remove vortex
-            vortices, _ = vortices.removeVortex(i)
-            break
-        }
-    }
+	//fmt.Println("Before merge")
 
-    //fmt.Println("Before merge")
+	// merge into super vertex
+	for i := range edges {
+		if edges[i].From == x.To {
+			edges[i].From = x.From
+		}
 
-    // merge into super vertex
-    for i := range edges {
-        if edges[i].From == x.To {
-            edges[i].From = x.From
-        }
+		if edges[i].To == x.To {
+			edges[i].To = x.From
+		}
+	}
 
-        if edges[i].To == x.To {
-            edges[i].To = x.From
-        }
-    }
+	if print {
+		fmt.Println("Edges after contraction")
+		fmt.Println(edges)
+	}
 
-    if print {
-        fmt.Println("Edges after contraction")
-        fmt.Println(edges)
-    }
+	//fmt.Println("After merge")
 
-    //fmt.Println("After merge")
+	// Remove self-loops
+	cross_edges := make(Edges, 0)
 
-    // Remove self-loops
-    cross_edges := make(Edges, 0)
+	for _, edge := range edges {
+		if edge.From != edge.To {
+			cross_edges = append(cross_edges, edge)
+		}
+	}
 
-    for _, edge := range edges {
-        if edge.From != edge.To {
-            cross_edges = append(cross_edges, edge)
-        }
-    }
+	if print {
+		fmt.Println("Crossing edges")
+		fmt.Println(cross_edges)
+	}
 
-    if print {
-        fmt.Println("Crossing edges")
-        fmt.Println(cross_edges)
-    }
-
-    return KargerContraction(vortices, cross_edges)
+	return KargerContraction(vortices, cross_edges)
 }
-
-
--- min_cut.go
+++ min_cut.go
@@ -1,75 +1,81 @@
 package main
 
 import (
-    "fmt"
-    "github.com/schmooser/algo-007/graph"
-    "github.com/schmooser/algo-007/util"
-    //"math/rand"
+	"fmt"
+
+	"github.com/schmooser/algo-007/graph"
+	"github.com/schmooser/algo-007/util"
 )
 
+/*
+func main() {
+	vortices := graph.Vortices{graph.Vortex{1}, graph.Vortex{2}, graph.Vortex{3}}
+	vortices = vortices.RemoveVortex(0)
+	fmt.Println(vortices)
+}
+*/
 
 func main() {
-    filename := "data/kargerMinCut.txt"
+	filename := "data/kargerMinCut.txt"
 
-    fmt.Printf("Importing %s\n", filename)
-    rows, _ := util.ReadRows(filename, " ")
+	fmt.Printf("Importing %s\n", filename)
+	rows, _ := util.ReadRows(filename, " ")
 
-    /*
-    var rows [][]int;
-    rows = append(rows, []int{1, 2, 3})
-    rows = append(rows, []int{2, 1, 4, 5})
-    rows = append(rows, []int{3, 1, 4})
-    rows = append(rows, []int{4, 3, 2, 6})
-    rows = append(rows, []int{5, 2, 7, 6})
-    rows = append(rows, []int{6, 5, 4, 8})
-    rows = append(rows, []int{7, 5, 8})
-    rows = append(rows, []int{8, 6, 5})
-    */
+	/*
+	   var rows [][]int;
+	   rows = append(rows, []int{1, 2, 3})
+	   rows = append(rows, []int{2, 1, 4, 5})
+	   rows = append(rows, []int{3, 1, 4})
+	   rows = append(rows, []int{4, 3, 2, 6})
+	   rows = append(rows, []int{5, 2, 7, 6})
+	   rows = append(rows, []int{6, 5, 4, 8})
+	   rows = append(rows, []int{7, 5, 8})
+	   rows = append(rows, []int{8, 6, 5})
+	*/
 
-    fmt.Println("MakeGraph")
-    vortices, edges := graph.MakeGraph(rows)
+	fmt.Println("MakeGraph")
+	vortices, edges := graph.MakeGraph(rows)
 
-    //fmt.Println(vortices)
-    //fmt.Println(edges)
+	//fmt.Println(vortices)
+	//fmt.Println(edges)
 
-    fmt.Println("KargerContraction")
+	fmt.Println("KargerContraction")
 
-    //rand.Seed(int64(164))
-    //mc_vortices, mc_edges := graph.KargerContraction(vortices, edges)
+	//rand.Seed(int64(164))
+	//mc_vortices, mc_edges := graph.KargerContraction(vortices, edges)
 
-    m := len(edges)
-    var vv graph.Vortices
-    var ee graph.Edges
+	m := len(edges)
+	var vv graph.Vortices
+	var ee graph.Edges
 
-    for i := 1; i <= len(vortices)*2; i++ {
-        //fmt.Printf("Iteration %d\n", i)
+	for i := 1; i <= len(vortices)*2; i++ {
+		//fmt.Printf("Iteration %d\n", i)
 
-        //rand.Seed(int64(i))
+		//rand.Seed(int64(i))
 
-        vv = make([]graph.Vortex, len(vortices))
-        copy(vv, vortices)
+		vv = make([]graph.Vortex, len(vortices))
+		copy(vv, vortices)
 
-        ee = make([]graph.Edge, len(edges))
-        copy(ee, edges)
+		ee = make([]graph.Edge, len(edges))
+		copy(ee, edges)
 
-        //mc_vortices, mc_edges := graph.KargerContraction(vv, ee)
-        _, mc_edges := graph.KargerContraction(vv, ee)
+		//mc_vortices, mc_edges := graph.KargerContraction(vv, ee)
+		_, mc_edges := graph.KargerContraction(vv, ee)
 
-        //fmt.Printf("MinCut on iteration %d = %d\n", i, len(mc_edges))
+		//fmt.Printf("MinCut on iteration %d = %d\n", i, len(mc_edges))
 
-        if len(mc_edges) < m {
-            m = len(mc_edges)
-        }
+		if len(mc_edges) < m {
+			m = len(mc_edges)
+		}
 
-        //fmt.Println(mc_vortices)
-        //fmt.Println(mc_edges)
+		//fmt.Println(mc_vortices)
+		//fmt.Println(mc_edges)
 
-    }
+	}
 
-    //fmt.Println(mc_vortices)
-    //fmt.Println(mc_edges)
+	//fmt.Println(mc_vortices)
+	//fmt.Println(mc_edges)
 
-    fmt.Printf("MinCut: %d\n", m)
-    fmt.Println("Done!")
+	fmt.Printf("MinCut: %d\n", m)
+	fmt.Println("Done!")
 }
-
